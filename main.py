c813bbc2d1feb04793df87a9b2af6d58f1ed07ad
Ishan Dewangan <ishan.dewangan2005@gmail.com>
2026-06-05 00:26:36 +0530

    Week 5: agent module setup + dependencies

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from database import engine, Base
 from routers import auth
 from models.transaction import Transaction
@@ -9,6 +10,14 @@ from routers import transactions
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(transactions.router, prefix="/transactions", tags=["Transactions"])
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
  
