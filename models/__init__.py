6b03551ce4adc22e48a10fba89a601584f6917d1
Ishan Dewangan <ishan.dewangan2005@gmail.com>
2026-06-19 16:23:27 +0530

    Add current_balance column to users table via Alembic migration

---
+++ models/__init__.py
@@ -0,0 +1,3 @@
+from .user import User
+from .transaction import Transaction
+from .conversation import Conversation
--- models/user.py
+++ models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String,DateTime
+from sqlalchemy import Column, Integer, String,DateTime,Float
 from sqlalchemy.sql import func
 from database import Base
 
@@ -10,3 +10,5 @@ class User(Base):
     email= Column(String, unique=True,index=True,nullable=False)
     password = Column(String, nullable=False)
     created_at = Column(DateTime(timezone=True),server_default=func.now())
+    current_balance = Column(Float, nullable=True,default=0.0)
+    
