0ad82f5faf250901d6f2fb24e9e6285bf493dd17
Ishan Dewangan <ishan.dewangan2005@gmail.com>
2026-07-14 16:33:56 +0530

    feat: add forecast vs actual accuracy endpoint

--- services/forecaster.py
+++ services/forecaster.py
@@ -2,6 +2,8 @@ from prophet import Prophet
 import pandas as pd
 from database import SessionLocal
 from models.transaction import Transaction
+from datetime import datetime
+from sqlalchemy import extract
 
 def get_forecast(user_id: int):
     db= SessionLocal()
@@ -33,5 +35,30 @@ def get_forecast(user_id: int):
 
     return{
         "predicted_total": total,
+        "currency": "INR"
+    }
+
+def get_forecast_accuracy(user_id: int):
+    db = SessionLocal()
+    now = datetime.now()
+    last_month = now.month - 1 if now.month > 1 else 12
+    last_month_year = now.year if now.month > 1 else now.year - 1
+
+    transactions = db.query(Transaction).filter(
+        Transaction.user_id == user_id,
+        Transaction.category != "Pass-Through",
+        extract('month', Transaction.created_at) == last_month,
+        extract('year', Transaction.created_at) == last_month_year
+    ).all()
+    db.close()
+
+    actual = round(sum(t.amount for t in transactions), 2)
+    predicted = get_forecast(user_id)["predicted_total"]
+    accuracy = round((1- abs(actual- predicted)/actual)*100, 2)
+
+    return{
+        "actual": actual,
+        "predicted": predicted,
+        "accuracy_percent": accuracy,
         "currency": "INR"
     }
